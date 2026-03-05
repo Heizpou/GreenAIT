627ccfecdca2763ae7dbc4ac78b153855498b851
Luke <lukeplaisgame@gmail.com>
2026-03-05 01:59:51 +0100

    feat: initialisation du CI/CD et tests sur le déploiement

--- server-simulator/runner.py
+++ server-simulator/runner.py
@@ -7,7 +7,7 @@ from server_lib.workload import Workload
 def main():
     # Récupération de l'id du serveur
     server_id = os.environ.get("SERVER_ID", "srv_default")
-    
+
     # Récupération du workload et création du serveur
     workload = Workload()
     server = Server(server_id)
@@ -15,10 +15,10 @@ def main():
 
     # Boucle pour faire vivre le serveur
     while True:
-        
+
         load = workload.get_global_load()
         server.step(load)
-        
+
         print("État actuel du serveur", server.id, " : ", server.cpu_usage_percent, "%")
         time.sleep(30)
 
