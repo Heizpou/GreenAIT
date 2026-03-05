482da82282d727100a6c15202e30163e10792ab8
Heizpou <104769697+Heizpou@users.noreply.github.com>
2026-03-05 23:58:54 +0100

    Modify HelloWorld message and add welcome text
    Updated the message in HelloWorld component and added a welcome paragraph.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,7 +11,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue - BIENVENUE" />
+  <HelloWorld msg="Vite + Vue" />
+  <p>Bienvenue sur le site WEB GreenAIT</p>
 </template>
 
 <style scoped>
